5bcf4f921c85699403124c2d70d55ffa9274e002
Nick Ng <nickngch@gmail.com>
2020-07-06 14:16:50 +0800

    added https

---
+++ bootstrap_https.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+	echo "Usage:"
+	echo "Option:"
+	echo "    1: Gitea"
+	echo "    2: Minio"
+	echo "    3: Elasticsearch"
+	echo "    4: Kibana"
+	echo "    5: Registry"
+        echo "    6: Elasticsearch + Kibana"
+    exit
+fi
+
+
+gitea() {
+echo "##### Installing Gitea"
+  docker-compose up -f docker-compose-https.yaml -d reverse-proxy gitea
+}
+
+minio() {
+echo "##### Installing Minio"
+  docker-compose up -f docker-compose-https.yaml -d reverse-proxy minio
+}
+
+elasticsearch() {
+echo "##### Installing Elasticsearch"
+  docker-compose up -f docker-compose-https.yaml  -d reverse-proxy elasticsearch
+  sysctl -w vm.max_map_count=262144
+}
+
+kibana() {
+echo "##### Installing Kibana"
+  docker-compose up -f docker-compose-https.yaml -d reverse-proxy kibana
+}
+
+registry() {
+echo "##### Installing Registry"
+  docker-compose up -f docker-compose-https.yaml -d reverse-proxy registry
+}
+
+ek() {
+echo "##### Installing Elasticsearch + Kibana"
+  docker-compose up -f docker-compose-https.yaml -d reverse-proxy elasticsearch kibana
+}
+
+
+case "$1" in
+1)	gitea
+	;;
+
+2)	minio
+	;;
+
+3)	elasticsearch
+	;;
+
+4)	kibana
+	;;
+
+5)	registry
+	;;
+
+6)	ek
+	;;
+
+*)	echo "Invalid option"
+	;;
+esac
